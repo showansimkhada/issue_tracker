2d1281669a53bc93a42677a4bebe6c1a5714b094
Showan <showansimkhada@hotmail.com>
2021-07-19 20:04:06 +1200

    Update connection

--- connection.js
+++ connection.js
@@ -1,8 +1,21 @@
-// Do not change this file
 require('dotenv').config();
-const MongoClient = require('mongodb');
+const { MongoClient } = require('mongodb');
 
+async function main(callback) {
+    const URI = process.env.MONGO_URI;
+    const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true});
 
-let db = MongoClient.connect(process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true});
+    try {
+        // Connect to MONGO cluster
+        await client.connect();
 
-module.exports = db;
+        // Make the appropriate DB callback
+        await callback(client);
+    } catch (e) {
+        // Catch any errors
+        console.log(e);
+        throw new Error("Unable to connect to database");
+    }
+}
+
+module.exports = main;
